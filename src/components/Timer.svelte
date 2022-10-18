d8767968aa4ea56a4abc53de6f9d8868d4812dfb
SimonDMCPlayer <46278840+SimonDMCPlayer@users.noreply.github.com>
2022-10-18 07:54:23 +0200

    Migrate to SvelteKit

---
+++ src/components/Timer.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	export let downScale: number = 1;
+
+	// size of one timer
+	let timerWidth = `${(1 / downScale) * 100}%`;
+	let timerHeight = `${(1 / downScale) * 100}%`;
+
+	// gap between timers
+	let gap = `${(0.2 * (downScale - 1)) / downScale}rem`;
+
+	// set size and font size of timer element
+	let sizeStyling = `
+        font-size: ${1 / downScale}rem;
+        width: calc(${timerWidth} - ${gap});
+        height: calc(${timerHeight} - ${gap});
+    `;
+</script>
+
+<div class="background" style={sizeStyling}>
+	<h1>Default Timer</h1>
+	<p>5:34</p>
+	<ul>
+		<li><button /></li>
+		<li><button /></li>
+	</ul>
+</div>
+
+<style>
+	.background {
+		background-color: #282c34;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	h1 {
+		color: white;
+		font-size: 4.5em;
+		font-family: 'Inter', sans-serif;
+		font-weight: 700;
+	}
+
+	p {
+		color: white;
+		font-size: 20.5em;
+		font-family: 'Inter', sans-serif;
+		font-weight: 200;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Timer from '../components/Timer.svelte';
+
+	const boardSize = 2;
+</script>
+
+<div class="app-background">
+	<div class="container">
+		{#each Array(boardSize ** 2) as _, i}
+			<Timer downScale={boardSize} />
+		{/each}
+	</div>
+</div>
+
+<style>
+	.app-background {
+		background-color: #23262c;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	.container {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 0.2rem;
+		width: 100vw;
+		max-width: 1310px;
+		height: 100vh;
+		background-color: white;
+		/* fix weird 1px white line */
+		margin-right: 1px;
+	}
+</style>
